6c6879fecfdaa58f8d9fb292b531106f686b90e7
Orlin_ <orlin221@abv.bg>
2023-10-20 09:55:53 +0300

    WordAnalyzer Project
    Firstly I create string variable and assign text from chitanka.info. Then I create words array that splits the text by char symbols. Including all the symbols, using removeEmptyEntries to remove all the space that is left. For better understanding I can structure the text with spaces, but they will be gone after I start the program. I create new list, the looped through the words array and if the length of the words is greater than or equal to 3 I add the word to the new list I created. Then I create an integer variable that I assign the count of the words in the list. Now I create method that is used to find the longest word in the text. It's string methods that loops with foreach loop through the words in the array. I assign  to null the new string variable longest. Then I check with simple if check the longest word using the operator or ||. if the check is successful I assign the longest variable to the word that is in the list. At the end of the method I return the longest. In new string variable I assign the method FindLongestWord with the name of the list inside. At the end the program prints the result of the longest word. Now working on the shortest and on the others methods.

---
+++ C# & TPL University/WordAnalyzer/WordAnalyzer/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace WordAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Seductive beauty Jodi loves money." +
+                " She is even ready to kill to make her dreams come true." +
+                "\r\n\r\nHer target is rich old men. He finds them on the Internet," +
+                " charms them and gets rid of them in an extremely exotic way." +
+                " And on her heels is a hired killer...\r\n\r\nDetective Roy Grace," +
+                " vigilant but haunted by shadows from the past, takes up the case." +
+                "\r\n\r\nWill Grace be able to capture Jody before another unsuspecting" +
+                " victim falls into her webs?";
+
+            string[] words = text.Split(new char[]{ ' ', '.', ',', '!', '?', ':', ';', '(', ')', '\n', '\r', '\t' },
+            StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> validTextWords = new List<string>();
+
+            foreach (string word in words)
+            {
+                if(word.Length>= 3)
+                {
+                    validTextWords.Add(word);
+                }
+                else
+                {
+
+                }
+            }
+
+
+            int nums = validTextWords.Count;
+
+            string longestW = FindLongestWord(validTextWords);
+
+            static string FindLongestWord(List<string> words)
+            {
+                string longest = null;
+                foreach (string word in words)
+                {
+                    if(longest == null || word.Length > longest.Length)
+                    {
+                        longest = word;
+                    }
+                }
+                return longest;
+            }
+
+            Console.WriteLine("Longest word {0}", longestW);
+
+        }
+    }
+}
