333cf1e710e738c745b54ec43cf0c009f77b3328
Orlin_ <orlin221@abv.bg>
2023-11-29 20:12:37 +0200

    Area 51
     The elevator moves to the chosen floor, and a security check determines whether the agent has clearance. If clearance is granted, a success message is displayed, and the simulation continues. The process repeats for a set number of agents, and the program concludes with a message indicating the facility is closed. Using threads -  agentThread.Join() and then Thread.Sleep(1000), after that elevatorThread.Join(), simulates waiting 1 second for the elevator to come.

---
+++ C# & TPL University/HomeWorkArea/HomeWorkArea/Entities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkArea
+{
+    public class Entities
+    {
+        public List<string> agentNames = new List<string> { "Agent1", "Agent2", "Agent3" };
+        public List<string> agentsClearenceLevels = new List<string> { "Confidential", "Secret", "Top-Secret" };
+    }
+}
---
+++ C# & TPL University/HomeWorkArea/HomeWorkArea/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Threading;
+
+namespace HomeWorkArea
+{
+    public class Program
+    {
+        static (string Name, string SecurityLevel) agent = ("", "");
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int currentFloor = 1;
+            Console.WriteLine($"Current floor {currentFloor}");
+
+            for (int i = 0; i < random.Next(1, 20); i++)
+            {
+                Console.WriteLine("Agent entered the elevator.");
+                Console.WriteLine();
+
+                Thread agentThread = new Thread(() => GenerateAgent());
+                agentThread.Start();
+                agentThread.Join();
+
+                Thread.Sleep(1000);
+
+                Thread elevatorThread = new Thread(() => PressElevator(agent.SecurityLevel, currentFloor));
+                elevatorThread.Start();
+                elevatorThread.Join();
+
+                Console.WriteLine("............................");
+            }
+
+            Console.WriteLine("The base is closed!");
+        }
+
+        public static void GenerateAgent()
+        {
+            Entities entities = new Entities();
+            var random = new Random();
+            agent.Name = entities.agentNames[random.Next(entities.agentNames.Count)];
+            agent.SecurityLevel = entities.agentsClearenceLevels[random.Next(entities.agentsClearenceLevels.Count)];
+            Console.WriteLine($"Agent name: {agent.Name}");
+            Console.WriteLine($"Security level: {agent.SecurityLevel}");
+        }
+
+        public static bool Check(string securityLevel, int floor)
+        {
+            bool securityCheckPassed = false;
+
+            Console.WriteLine();
+
+            switch (securityLevel)
+            {
+                case "Confidential":
+                    securityCheckPassed = (floor == 1);
+                    break;
+
+                case "Secret":
+                    securityCheckPassed = (floor == 1 || floor == 2);
+                    break;
+
+                case "Top-Secret":
+                    securityCheckPassed = (floor >= 1 && floor <= 4);
+                    break;
+            }
+
+            if (securityCheckPassed)
+            {
+                Console.WriteLine($"Access granted! The elevator door glides open, ushering {agent.Name} out of the cabin.");
+            }
+            else
+            {
+                Console.WriteLine("Access denied! Please select a different floor for your destination.");
+            }
+
+            Console.WriteLine();
+
+            return securityCheckPassed;
+        }
+
+        private static int GetSelectedFloor()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1, 5);
+        }
+
+        private static void MoveToFloor(int currentFloor, int selectedFloor)
+        {
+            for (int j = 0; j < Math.Abs(selectedFloor - currentFloor); j++)
+            {
+                Console.WriteLine();
+                Console.Write("-");
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void PressElevator(string securityLevel, int currentFloor)
+        {
+            int selectedFloor;
+            bool securityCheckPassed;
+
+            do
+            {
+                Console.WriteLine("Choose floor: G(1), S(2), T1(3), T2(4)");
+                selectedFloor = GetSelectedFloor();
+
+                Console.Write($"Arriving at the floor {selectedFloor} ");
+                MoveToFloor(currentFloor, selectedFloor);
+
+                Console.WriteLine();
+                currentFloor = selectedFloor;
+                Console.WriteLine($"Arrived at floor {currentFloor}");
+
+                securityCheckPassed = Check(securityLevel, selectedFloor);
+
+            } while (!securityCheckPassed);
+        }
+
+    }
+}
